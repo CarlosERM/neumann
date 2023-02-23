ba1765516210b45b24ba974d0d12b86eb214abe1
Carlos Eduardo <carlosermiranda.dev@gmail.com>
2023-02-23 13:08:00 -0300

    flutter_launcher icons foi adicionado

--- neumann_flutter/lib/main.dart
+++ neumann_flutter/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'Home.dart';
-import "theme.dart";
+import '/view/home.dart';
+import 'util/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Neumann',
         theme: theme,
         home: const Home(),
         debugShowCheckedModeBanner: false);
---
+++ neumann_flutter/lib/util/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme = ThemeData(
+    fontFamily: 'Poppins',
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
+      displayMedium: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+      displaySmall: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+      headlineLarge: TextStyle(),
+      headlineMedium: TextStyle(),
+      headlineSmall: TextStyle(),
+      titleLarge: TextStyle(),
+      titleMedium: TextStyle(),
+      titleSmall: TextStyle(),
+      bodyLarge: TextStyle(),
+      bodyMedium: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+      bodySmall: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+      labelSmall: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+    ));
